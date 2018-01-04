0289e9a50f157138145747b3b2be6509d7ae4cdc
Jesús Mejía <jesusmejded@gmail.com>
2018-01-04 12:14:52 -0600

    Constrained Single Objective Real-Parameter Optimization

--- src/CEC17.jl
+++ src/CEC17.jl
@@ -1,8 +1,10 @@
 module CEC17
 
     export cec17_test_func
+    export cec17_test_COP
     const localDir = string(@__DIR__)
     const LIB = "$localDir/cfunctions.so"
+    const LIB_COP = "$localDir/cfunctions_cop.so"
 
 
     function cec17_test_func(x::Vector, func_num::Int)
@@ -16,6 +18,23 @@ module CEC17
                                    Int32, Int32, Int32),
                                    x, f, localDir, D, 1, func_num)
         return f[1]
+    end
+
+    function cec17_test_COP(x::Vector, func_num::Int)
+
+        D = length(x)
+        f = [0.0]
+        g = [0.0]
+        h = [0.0]
+
+        ccall((:func, LIB_COP), Void, (Ptr{Cdouble},
+                                   Ptr{Cdouble},
+                                   Ptr{Cdouble},
+                                   Ptr{Cdouble},
+                                   Ptr{Cchar},
+                                   Int32, Int32, Int32),
+                                   x, f, g, h, localDir, D, 1, func_num)
+        return f[1], g[1], h[1]
     end
 
 end # module
